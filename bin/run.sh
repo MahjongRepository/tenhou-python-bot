138f56cae7ef71fc15979da0a6c7fa799a50bee3
Alexey Lisikhin <lisikhin@gmail.com>
2016-05-19 23:31:31 +0800

    Update run script

--- bin/run.sh
+++ bin/run.sh
@@ -7,6 +7,14 @@
 # */5 * * * * bash /home/bot/app/bin/run.sh
 
 PID=`ps -eaf | grep project/main.py | grep -v grep | awk '{print $2}'`
+
+
 if [[ "" =  "$PID" ]]; then
   /home/bot/env/bin/python /home/bot/app/project/main.py
+else
+  WORKED_SECONDS=`ps -p "$PID" -o etimes=`
+  # if process run > 60 minutes, probably it hand and we need to kill it
+  if [[ ${WORKED_SECONDS} > 3600 ]]; then
+    kill -9 ${PID}
+  fi
 fi

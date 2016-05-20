d2deec3decc1eb1d7f0f0a17f718b84982e12d35
Alexey Lisikhin <lisikhin@gmail.com>
2016-05-20 15:27:39 +0800

    Update run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,19 +1,11 @@
 #!/bin/sh -e
 
-# cron will run each 5 minutes
-# and will search the run process
-# if there is no process, it will run it
-
-# */5 * * * * bash /home/bot/app/bin/run.sh
-
+# kill old process if it hung
+# usually old process will be already ended
 PID=`ps -eaf | grep project/main.py | grep -v grep | awk '{print $2}'`
-
-if [[ "" =  "$PID" ]]; then
-  /home/bot/env/bin/python /home/bot/app/project/main.py
-else
-  WORKED_SECONDS=`ps -p "$PID" -o etimes=`
-  # if process run > 60 minutes, probably it hung and we need to kill it
-  if [[ ${WORKED_SECONDS} > 3600 ]]; then
-    kill -9 ${PID}
-  fi
+if [[ "" !=  "$PID" ]]; then
+  echo "killing $PID"
+  kill -9 $PID
 fi
+
+/home/bot/env/bin/python /home/bot/app/project/main.py

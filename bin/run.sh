8133cdcbb16aa1a0150b7cf20ffd99465a7ff18a
Alexey Lisikhin <lisikhin@gmail.com>
2016-05-20 21:01:32 +0800

    Fix run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -e
 
-# kill old process if it hang
-# usually old process will be already ended
-PID=`ps -eaf | grep project/main.py | grep -v grep | awk '{print $2}'`
-if [[ "" !=  "$PID" ]]; then
-  kill -9 ${PID}
-fi
+# cron will run each 5 minutes
+# and will search the run process
+# if there is no process, it will run it
 
-sleep 20
-/home/bot/env/bin/python /home/bot/app/project/main.py
+# */5 * * * * bash /home/bot/app/bin/run.sh
+
+PID=`ps -eaf | grep project/main.py | grep -v grep | awk '{print $2}'`
+
+if [[ "" =  "$PID" ]]; then
+  /home/bot/env/bin/python /home/bot/app/project/main.py
+else
+  WORKED_SECONDS=`ps -p "$PID" -o etimes=`
+  # if process run > 60 minutes, probably it hang and we need to kill it
+  if [ ${WORKED_SECONDS} -gt "3600" ]; then
+    echo "run"
+  fi
+fi

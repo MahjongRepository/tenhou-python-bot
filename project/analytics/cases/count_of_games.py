1217bc8fed203953d02a041b9dc4d00c7059373d
Alexey Lisikhin <lisikhin@gmail.com>
2017-03-03 10:31:14 +0800

    Update analytics process

--- project/analytics/cases/count_of_games.py
+++ project/analytics/cases/count_of_games.py
@@ -1,47 +1,39 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 from datetime import datetime
+
+import logging
+
 from analytics.cases.main import ProcessDataCase
+
+logger = logging.getLogger('process')
 
 
 class CountOfGames(ProcessDataCase):
 
     def process(self):
-        years = range(2009, datetime.now().year + 1)
-
         connection = sqlite3.connect(self.db_file)
 
         with connection:
             cursor = connection.cursor()
 
-            tonpusen_games_array = []
-            hanchan_games_array = []
+            total_games_sql = 'SELECT count(*) from logs'
+            hanchan_games_sql = 'SELECT count(*) from logs where is_tonpusen = 0;'
 
-            for year in years:
-                total_games_sql = 'SELECT count(*) from logs where year = {};'.format(year)
-                hanchan_games_sql = 'SELECT count(*) from logs where year = {} and is_tonpusen = 0;'.format(year)
-                tonpusen_games_sql = 'SELECT count(*) from logs where year = {} and is_tonpusen = 1;'.format(year)
+            cursor.execute(total_games_sql)
+            data = cursor.fetchone()
+            total_games = data and data[0] or 0
 
-                cursor.execute(total_games_sql)
-                data = cursor.fetchone()
-                total_games = data and data[0] or 0
+            cursor.execute(hanchan_games_sql)
+            data = cursor.fetchone()
+            hanchan_games = data and data[0] or 0
 
-                cursor.execute(hanchan_games_sql)
-                data = cursor.fetchone()
-                hanchan_games = data and data[0] or 0
+            tonpusen_games = total_games - hanchan_games
 
-                cursor.execute(tonpusen_games_sql)
-                data = cursor.fetchone()
-                tonpusen_games = data and data[0] or 0
+            hanchan_percentage = total_games and (hanchan_games / total_games) * 100 or 0
+            tonpusen_percentage = total_games and (tonpusen_games / total_games) * 100 or 0
 
-                hanchan_percentage = total_games and (hanchan_games / total_games) * 100 or 0
-                tonpusen_percentage = total_games and (tonpusen_games / total_games) * 100 or 0
-
-                print(year)
-                print('Total games: {}'.format(total_games))
-                print('Hanchan games: {}, {:.2f}%'.format(hanchan_games, hanchan_percentage))
-                print('Tonpusen games: {}, {:.2f}%'.format(tonpusen_games, tonpusen_percentage))
-                print()
-
-                tonpusen_games_array.append(tonpusen_percentage)
-                hanchan_games_array.append(hanchan_percentage)
+            logger.info('Total games: {}'.format(total_games))
+            logger.info('Hanchan games: {}, {:.2f}%'.format(hanchan_games, hanchan_percentage))
+            logger.info('Tonpusen games: {}, {:.2f}%'.format(tonpusen_games, tonpusen_percentage))
+            logger.info('')
--- project/analytics/cases/honitsu_hands.py
+++ project/analytics/cases/honitsu_hands.py
@@ -1,33 +1,41 @@
 # -*- coding: utf-8 -*-
+import re
+
+import logging
+
 from analytics.cases.main import ProcessDataCase
-from analytics.tags import AGARI_TAG
-from mahjong.tile import TilesConverter
+
+logger = logging.getLogger('process')
 
 
 class HonitsuHands(ProcessDataCase):
-    HONITSU_ID = 34
+    HONITSU_ID = '34'
 
     def process(self):
         self.load_all_records()
-        filtered = self.filter_hands()
-        for item in filtered:
-            meld_tiles = []
-            if item.parsed_melds:
-                for meld in item.parsed_melds:
-                    tiles = meld.tiles
-                    if meld.type == meld.KAN or meld.type == meld.CHAKAN:
-                        tiles = tiles[:3]
-                    meld_tiles.extend(tiles)
 
-            closed_hand_tiles = [int(x) for x in item.closed_hand.split(',')]
-            tiles = closed_hand_tiles + meld_tiles
-            print(TilesConverter.to_one_line_string(tiles))
+        filtered_rounds = self.filter_rounds()
+        logger.info('Found {} honitsu hands'.format(len(filtered_rounds)))
 
-    def filter_hands(self):
-        filtered = []
+    def filter_rounds(self):
+        """
+        Find all rounds that were ended with honitsu hand
+        """
+        filtered_rounds = []
+
+        total_rounds = []
         for hanchan in self.hanchans:
-            for tag in hanchan.tags:
-                if tag.tag_name == AGARI_TAG:
-                    if self.HONITSU_ID in tag.parsed_yaku:
-                        filtered.append(tag)
-        return filtered
+            total_rounds.extend(hanchan.rounds)
+
+        find = re.compile(r'yaku=\"(.+?)\"')
+        for round_item in total_rounds:
+            for tag in round_item:
+                if 'AGARI' in tag and 'yaku=' in tag:
+                    yaku_temp = find.findall(tag)[0].split(',')
+                    # start at the beginning at take every second item (even)
+                    yaku_list = yaku_temp[::2]
+
+                    if self.HONITSU_ID in yaku_list:
+                        filtered_rounds.append(round_item)
+
+        return filtered_rounds

dc0129224dc01f4e9cdaa57ee2aff307a4f5d7d3
Alexey Lisikhin <lisikhin@gmail.com>
2017-03-10 18:36:32 +0800

    Add hash from the bot name to the log name

--- project/utils/logger.py
+++ project/utils/logger.py
@@ -2,17 +2,23 @@
 
 import datetime
 import logging
-
 import os
+import hashlib
+
+from utils.settings_handler import settings
 
 
 def set_up_logging():
     """
-    Main logger for usual bot needs
+    Logger for tenhou communication and AI output
     """
     logs_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'logs')
     if not os.path.exists(logs_directory):
         os.mkdir(logs_directory)
+
+    # we shouldn't be afraid about collision
+    # also, we need it to distinguish different bots logs (if they were run in the same time)
+    name_hash = hashlib.sha1(settings.USER_ID.encode('utf-8')).hexdigest()[:5]
 
     logger = logging.getLogger('tenhou')
     logger.setLevel(logging.DEBUG)
@@ -20,7 +26,7 @@ def set_up_logging():
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
 
-    file_name = datetime.datetime.now().strftime('%Y-%m-%d %H_%M_%S') + '.log'
+    file_name = '{}_{}.log'.format(name_hash, datetime.datetime.now().strftime('%Y-%m-%d %H_%M_%S'))
     fh = logging.FileHandler(os.path.join(logs_directory, file_name))
     fh.setLevel(logging.DEBUG)
 

095f2a78416b12f23b4f59d6415103bd4873a86b
Alexey Lisikhin <lisikhin@gmail.com>
2017-04-12 09:01:18 +0800

    Handle unexpected errors and store stack trace to the log

--- project/tenhou/main.py
+++ project/tenhou/main.py
@@ -24,3 +24,7 @@ def connect_and_play():
     except KeyboardInterrupt:
         logger.info('Ending the game...')
         client.end_game()
+    except Exception as e:
+        logger.exception('Unexpected exception', exc_info=e)
+        logger.info('Ending the game...')
+        client.end_game(False)

94e8eb32bfd20ae3b6bfa10296072321686a0e29
Alexey Lisikhin <lisikhin@gmail.com>
2017-06-22 22:23:56 +0800

    Display AI version

--- project/tenhou/main.py
+++ project/tenhou/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
+from mahjong.ai.main import MainAI
 from tenhou.client import TenhouClient
 from utils.settings_handler import settings
 
@@ -10,6 +11,8 @@ logger = logging.getLogger('tenhou')
 
 def connect_and_play():
     logger.info('Bot AI enabled: {}'.format(settings.ENABLE_AI))
+    if settings.ENABLE_AI:
+        logger.info('AI version: {}'.format(MainAI.version))
 
     client = TenhouClient()
     client.connect()

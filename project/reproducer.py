b4bdf590d73c3b495a934a9948e2c6964a2e896a
Alexey <lisikhin@gmail.com>
2018-08-25 19:26:13 +0800

    Various small fixes for log reproducer (#63)
    * Don't save log reproducer output to the file
    * Introduce log name prefix setting
    * Improve tenhou reproducer output

--- project/reproducer.py
+++ project/reproducer.py
@@ -41,8 +41,26 @@ class TenhouLogReproducer(object):
 
         table = Table()
         for tag in self.round_content:
+            if player_draw_regex.match(tag) and 'UN' not in tag:
+                print('Player draw')
+                tile = self.decoder.parse_tile(tag)
+                table.player.draw_tile(tile)
+
             if dry_run:
-                print(tag)
+                if self._is_draw(tag):
+                    print('<-', TilesConverter.to_one_line_string([self._parse_tile(tag)]), tag)
+                elif self._is_discard(tag):
+                    print('->', TilesConverter.to_one_line_string([self._parse_tile(tag)]), tag)
+                elif self._is_init_tag(tag):
+                    hands = {
+                        0: [int(x) for x in self._get_attribute_content(tag, 'hai0').split(',')],
+                        1: [int(x) for x in self._get_attribute_content(tag, 'hai1').split(',')],
+                        2: [int(x) for x in self._get_attribute_content(tag, 'hai2').split(',')],
+                        3: [int(x) for x in self._get_attribute_content(tag, 'hai3').split(',')],
+                    }
+                    print('Initial hand:', TilesConverter.to_one_line_string(hands[self.player_position]))
+                else:
+                    print(tag)
 
             if not dry_run and tag == self.stop_tag:
                 break
@@ -71,10 +89,6 @@ class TenhouLogReproducer(object):
                 ]
 
                 table.player.init_hand(hands[self.player_position])
-
-            if player_draw_regex.match(tag) and 'UN' not in tag:
-                tile = self.decoder.parse_tile(tag)
-                table.player.draw_tile(tile)
 
             if discard_regex.match(tag) and 'DORA' not in tag:
                 tile = self.decoder.parse_tile(tag)
@@ -198,6 +212,35 @@ class TenhouLogReproducer(object):
 
         return rounds[1:]
 
+    def _is_discard(self, tag):
+        skip_tags = ['<GO', '<FURITEN', '<DORA']
+        if any([x in tag for x in skip_tags]):
+            return False
+
+        match_discard = re.match(r"^<[defgDEFG]+\d*", tag)
+        if match_discard:
+            return True
+
+        return False
+
+    def _is_draw(self, tag):
+        match_discard = re.match(r"^<[tuvwTUVW]+\d*", tag)
+        if match_discard:
+            return True
+
+        return False
+
+    def _parse_tile(self, tag):
+        result = re.match(r'^<[defgtuvwDEFGTUVW]+\d*', tag).group()
+        return int(result[2:])
+
+    def _is_init_tag(self, tag):
+        return tag and 'INIT' in tag
+
+    def _get_attribute_content(self, tag, attribute_name):
+        result = re.findall(r'{}="([^"]*)"'.format(attribute_name), tag)
+        return result and result[0] or None
+
 
 class SocketMock(object):
     """
@@ -292,7 +335,7 @@ def parse_args_and_start_reproducer():
         reproducer = TenhouLogReproducer(opts.online_log, opts.tag)
         reproducer.reproduce(opts.dry_run)
     else:
-        set_up_logging()
+        set_up_logging(save_to_file=False)
 
         client = TenhouClient(SocketMock(opts.local_log))
         try:
--- project/utils/logger.py
+++ project/utils/logger.py
@@ -8,7 +8,7 @@ import hashlib
 from utils.settings_handler import settings
 
 
-def set_up_logging():
+def set_up_logging(save_to_file=True):
     """
     Logger for tenhou communication and AI output
     """
@@ -16,28 +16,30 @@ def set_up_logging():
     if not os.path.exists(logs_directory):
         os.mkdir(logs_directory)
 
-    # we shouldn't be afraid about collision
-    # also, we need it to distinguish different bots logs (if they were run in the same time)
-    name_hash = hashlib.sha1(settings.USER_ID.encode('utf-8')).hexdigest()[:5]
-
     logger = logging.getLogger('tenhou')
     logger.setLevel(logging.DEBUG)
 
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
 
-    file_name = '{}_{}.log'.format(name_hash, datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S'))
-    fh = logging.FileHandler(os.path.join(logs_directory, file_name), encoding='utf-8')
-    fh.setLevel(logging.DEBUG)
-
     formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     ch.setFormatter(formatter)
-    fh.setFormatter(formatter)
 
     logger.addHandler(ch)
-    logger.addHandler(fh)
 
     logger = logging.getLogger('ai')
     logger.setLevel(logging.DEBUG)
     logger.addHandler(ch)
-    logger.addHandler(fh)
+
+    if save_to_file:
+        # we need it to distinguish different bots logs (if they were run in the same time)
+        log_prefix = settings.LOG_PREFIX
+        if not log_prefix:
+            log_prefix = hashlib.sha1(settings.USER_ID.encode('utf-8')).hexdigest()[:5]
+
+        file_name = '{}_{}.log'.format(log_prefix, datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S'))
+        fh = logging.FileHandler(os.path.join(logs_directory, file_name), encoding='utf-8')
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+        logger.addHandler(fh)

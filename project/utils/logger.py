e9ec4fc55b98be41ae7103d9ebb5138db886f85f
Nihisil <lisikhin@gmail.com>
2018-08-28 19:43:48 +0800

    Fix file logging

--- project/utils/logger.py
+++ project/utils/logger.py
@@ -21,25 +21,23 @@ def set_up_logging(save_to_file=True):
 
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-
     formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     ch.setFormatter(formatter)
-
     logger.addHandler(ch)
 
-    logger = logging.getLogger('ai')
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(ch)
+    ai_logger = logging.getLogger('ai')
+    ai_logger.setLevel(logging.DEBUG)
+    ai_logger.addHandler(ch)
 
     if save_to_file:
         # we need it to distinguish different bots logs (if they were run in the same time)
         log_prefix = settings.LOG_PREFIX
         if not log_prefix:
             log_prefix = hashlib.sha1(settings.USER_ID.encode('utf-8')).hexdigest()[:5]
-
         file_name = '{}_{}.log'.format(log_prefix, datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S'))
+
         fh = logging.FileHandler(os.path.join(logs_directory, file_name), encoding='utf-8')
         fh.setLevel(logging.DEBUG)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
-        logger.addHandler(fh)
+        ai_logger.addHandler(fh)

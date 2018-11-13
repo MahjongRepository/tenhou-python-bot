9f4fbf76a18b4dc2aa02b1f1a531681ce0ff327f
bogachev-pa <bogachev.pa@gmail.com>
2018-11-14 02:02:22 +0300

    update yakuhai strategy logic after changing when tile is added to table discards

--- project/game/ai/first_version/tests/strategies/tests_yakuhai.py
+++ project/game/ai/first_version/tests/strategies/tests_yakuhai.py
@@ -268,13 +268,12 @@ class YakuhaiStrategyTestCase(unittest.TestCase, TestMixin):
 
         # let's skip first yakuhai early in the game
         tile = self._string_to_136_tile(honors='7')
-        # when we are melding tile it's already counted as discarded
-        self.table.add_discarded_tile(1, tile, False)
         meld, _ = self.player.try_to_call_meld(tile, True)
         self.assertEqual(meld, None)
 
-        # now the second one is out
+        # now one is out
         self.table.add_discarded_tile(1, tile, False)
+
         meld, _ = self.player.try_to_call_meld(tile, True)
         self.assertNotEqual(meld, None)
         self.assertEqual(self._to_string(meld.tiles), '777z')

6d1a6123e01b84161e573aace5f5f92a8bca205b
bogachev-pa <bogachev.pa@gmail.com>
2020-11-07 03:08:04 +0300

    consider tanyao impossible if there are non-suitable melds

--- project/game/ai/defence/yaku_analyzer/tanyao.py
+++ project/game/ai/defence/yaku_analyzer/tanyao.py
@@ -12,18 +12,22 @@ class TanyaoAnalyzer(YakuAnalyzer):
         return {"id": self.id}
 
     def is_yaku_active(self):
-        return len(self.get_suitable_melds()) > 0
+        return len(self._get_suitable_melds()) > 0
 
     def melds_han(self):
-        return len(self.get_suitable_melds()) > 0 and 1 or 0
+        return len(self._get_suitable_melds()) > 0 and 1 or 0
 
-    def get_suitable_melds(self):
+    def _get_suitable_melds(self):
         suitable_melds = []
         for meld in self.player.melds:
             tiles_34 = [x // 4 for x in meld.tiles]
             not_suitable_tiles = TERMINAL_INDICES + HONOR_INDICES
             if not any(x in not_suitable_tiles for x in tiles_34):
                 suitable_melds.append(meld)
+            else:
+                # if there is an unsuitable meld we consider tanyao impossible
+                return []
+
         return suitable_melds
 
     def get_safe_tiles_34(self):
--- project/game/ai/defence/yaku_analyzer/yakuhai.py
+++ project/game/ai/defence/yaku_analyzer/yakuhai.py
@@ -11,18 +11,18 @@ class YakuhaiAnalyzer(YakuAnalyzer):
         return {"id": self.id}
 
     def is_yaku_active(self):
-        return len(self.get_suitable_melds()) > 0
+        return len(self._get_suitable_melds()) > 0
 
     def melds_han(self):
         han = 0
-        suitable_melds = self.get_suitable_melds()
+        suitable_melds = self._get_suitable_melds()
         for x in suitable_melds:
             tile_34 = x.tiles[0] // 4
             # we need to do that to support double winds yakuhais
             han += len([x for x in self.player.valued_honors if x == tile_34])
         return han
 
-    def get_suitable_melds(self):
+    def _get_suitable_melds(self):
         suitable_melds = []
         for x in self.player.melds:
             tile_34 = x.tiles[0] // 4

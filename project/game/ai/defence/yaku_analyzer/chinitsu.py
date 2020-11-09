f5ea5d535734ce41cb2e1deb8ad2b88fc9b9eb20
bogachev-pa <bogachev.pa@gmail.com>
2020-11-09 05:58:32 +0300

    deduplicate honitsu/chinitsu analyzer code

--- project/game/ai/defence/yaku_analyzer/chinitsu.py
+++ project/game/ai/defence/yaku_analyzer/chinitsu.py
@@ -1,24 +1,16 @@
-from game.ai.defence.yaku_analyzer.yaku_analyzer import YakuAnalyzer
+from game.ai.defence.yaku_analyzer.honitsu_analyzer_base import HonitsuAnalyzerBase
 from mahjong.tile import TilesConverter
 from mahjong.utils import count_tiles_by_suits, is_honor
 
 
-class ChinitsuAnalyzer(YakuAnalyzer):
+class ChinitsuAnalyzer(HonitsuAnalyzerBase):
     id = "chinitsu"
-    chosen_suit = None
 
     MIN_DISCARD = 5
     MIN_DISCARD_FOR_LESS_SUIT = 10
     MAX_MELDS = 3
     EARLY_DISCARD_DIVISOR = 3
     LESS_SUIT_PERCENTAGE_BORDER = 30
-
-    def __init__(self, enemy):
-        self.enemy = enemy
-        self.chosen_suit = None
-
-    def serialize(self):
-        return {"id": self.id, "chosen_suit": self.chosen_suit and self.chosen_suit.__name__}
 
     def is_yaku_active(self):
         # TODO: in some distant future we may want to analyze menchin as well
@@ -49,32 +41,8 @@ class ChinitsuAnalyzer(YakuAnalyzer):
 
         assert current_suit
 
-        # now let's check the following considiton:
-        # if enemy had discarded tiles from that suit and after that he had discarded a tile from a different
-        # suit from his hand - let's believe it's not chinitsu
-        suit_discards_positions = [
-            self.enemy.discards.index(x) for x in self.enemy.discards if current_suit["function"](x.value // 4)
-        ]
-        if suit_discards_positions:
-            # we consider second discard of chosen suit to be reference point
-            # first one could have happened when player was not yet sure if he is going to honitsu
-            # after the second one there should be no discars of other suit from hand
-            reference_discard = suit_discards_positions[min(1, len(suit_discards_positions) - 1)]
-            discards_after = self.enemy.discards[reference_discard:]
-            if discards_after:
-                has_discarded_other_suit_from_hand = [
-                    x.value
-                    for x in discards_after
-                    if (
-                        not x.is_tsumogiri and not is_honor(x.value // 4) and not current_suit["function"](x.value // 4)
-                    )
-                ]
-                if has_discarded_other_suit_from_hand:
-                    return False
-
-            # if we started discards suit tiles early, it's probably not chinitsu
-            if suit_discards_positions[0] <= total_discards / ChinitsuAnalyzer.EARLY_DISCARD_DIVISOR:
-                return False
+        if not self._check_discard_order(current_suit, int(total_discards / ChinitsuAnalyzer.EARLY_DISCARD_DIVISOR)):
+            return False
 
         # finally let's check if discard is not too full of chosen suit
         if total_discards >= ChinitsuAnalyzer.MIN_DISCARD_FOR_LESS_SUIT:
@@ -111,15 +79,6 @@ class ChinitsuAnalyzer(YakuAnalyzer):
                 safe_tiles.append(x)
 
         return safe_tiles
-
-    def get_tempai_probability_modifier(self):
-        # if enemy has not yet discarded his suit and there are less than 3 melds, consider tempai less probable
-        suit_discards = [x for x in self.enemy.discards if self.chosen_suit(x.value // 4)]
-
-        if not suit_discards and len(self.enemy.melds):
-            return 0.5
-
-        return 1
 
     @staticmethod
     # FIXME: remove this method and use proper one from mahjong lib
--- project/game/ai/defence/yaku_analyzer/honitsu.py
+++ project/game/ai/defence/yaku_analyzer/honitsu.py
@@ -1,27 +1,19 @@
 from game.ai.defence.yaku_analyzer.chinitsu import ChinitsuAnalyzer
-from game.ai.defence.yaku_analyzer.yaku_analyzer import YakuAnalyzer
+from game.ai.defence.yaku_analyzer.honitsu_analyzer_base import HonitsuAnalyzerBase
 from game.ai.helpers.defence import TileDanger
 from mahjong.constants import HONOR_INDICES
 from mahjong.tile import TilesConverter
 from mahjong.utils import count_tiles_by_suits, is_honor
 
 
-class HonitsuAnalyzer(YakuAnalyzer):
+class HonitsuAnalyzer(HonitsuAnalyzerBase):
     id = "honitsu"
-    chosen_suit = None
 
     MIN_DISCARD = 6
     MAX_MELDS = 3
     EARLY_DISCARD_DIVISOR = 4
     LESS_SUIT_PERCENTAGE_BORDER = 20
     HONORS_PERCENTAGE_BORDER = 30
-
-    def __init__(self, enemy):
-        self.enemy = enemy
-        self.chosen_suit = None
-
-    def serialize(self):
-        return {"id": self.id, "chosen_suit": self.chosen_suit and self.chosen_suit.__name__}
 
     def is_yaku_active(self):
         # TODO: in some distant future we may want to analyze menhon as well
@@ -79,32 +71,8 @@ class HonitsuAnalyzer(YakuAnalyzer):
         if not current_suit:
             return False
 
-        # now let's check the following considiton:
-        # if enemy had discarded tiles from that suit or honor and after that he had discarded a tile from a different
-        # suit from his hand - let's believe it's not honitsu
-        suit_discards_positions = [
-            self.enemy.discards.index(x) for x in self.enemy.discards if current_suit["function"](x.value // 4)
-        ]
-        if suit_discards_positions:
-            # we consider second discard of chosen suit to be reference point
-            # first one could have happened when player was not yet sure if he is going to honitsu
-            # after the second one there should be no discars of other suit from hand
-            reference_discard = suit_discards_positions[min(1, len(suit_discards_positions) - 1)]
-            discards_after = self.enemy.discards[reference_discard:]
-            if discards_after:
-                has_discarded_other_suit_from_hand = [
-                    x
-                    for x in discards_after
-                    if (
-                        not x.is_tsumogiri and not is_honor(x.value // 4) and not current_suit["function"](x.value // 4)
-                    )
-                ]
-                if has_discarded_other_suit_from_hand:
-                    return False
-
-            # if we started discards suit tiles early, it's probably not honitsu
-            if suit_discards_positions[0] <= total_discards / HonitsuAnalyzer.EARLY_DISCARD_DIVISOR:
-                return False
+        if not self._check_discard_order(current_suit, int(total_discards / HonitsuAnalyzer.EARLY_DISCARD_DIVISOR)):
+            return False
 
         # all checks have passed - assume this is honitsu
         self.chosen_suit = current_suit["function"]
@@ -136,12 +104,3 @@ class HonitsuAnalyzer(YakuAnalyzer):
                 return [TileDanger.HONITSU_FIRST_SECOND_HONOR_BONUS_DANGER]
 
         return []
-
-    def get_tempai_probability_modifier(self):
-        # if enemy has not yet discarded his suit and there are less than 3 melds, consider tempai less probable
-        suit_discards = [x for x in self.enemy.discards if self.chosen_suit(x.value // 4)]
-
-        if not suit_discards and len(self.enemy.melds):
-            return 0.5
-
-        return 1

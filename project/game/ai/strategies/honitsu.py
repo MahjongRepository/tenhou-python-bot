046c301fcf7654cccc8c50101ea2d4e5d3916420
bogachev-pa <bogachev.pa@gmail.com>
2020-11-17 20:34:37 +0300

    make honitsu rules more strict

--- project/game/ai/strategies/honitsu.py
+++ project/game/ai/strategies/honitsu.py
@@ -45,6 +45,7 @@ class HonitsuStrategy(BaseStrategy):
 
         count_of_shuntsu_other_suits = 0
         count_of_koutsu_other_suits = 0
+        count_of_ryanmen_other_suits = 0
 
         count_of_shuntsu_other_suits += self._count_of_shuntsu(tiles_34, suits[1]["function"])
         count_of_shuntsu_other_suits += self._count_of_shuntsu(tiles_34, suits[2]["function"])
@@ -52,11 +53,18 @@ class HonitsuStrategy(BaseStrategy):
         count_of_koutsu_other_suits += self._count_of_koutsu(tiles_34, suits[1]["function"])
         count_of_koutsu_other_suits += self._count_of_koutsu(tiles_34, suits[2]["function"])
 
+        count_of_ryanmen_other_suits += self._find_ryanmen_waits(tiles_34, suits[1]["function"])
+        count_of_ryanmen_other_suits += self._find_ryanmen_waits(tiles_34, suits[2]["function"])
+
         self._calculate_suitable_and_not_suitable_tiles_cnt(tiles_34, suit["function"])
         self._initialize_honitsu_dora_count(tiles_136, suit)
 
         # let's not go for honitsu if we have 5 or more tiles in other suits
         if self.tiles_count_other_suits >= 5:
+            return False
+
+        # 7th turn and still 4 tiles in other suits - meh
+        if self.tiles_count_other_suits >= 4 and self.player.round_step > 6:
             return False
 
         # 12th turn is too late and we still have too many tiles in other suits
@@ -93,6 +101,10 @@ class HonitsuStrategy(BaseStrategy):
         if honor_pairs_or_pons == 0:
             return False
 
+        # if we have twu ryanmens in other suits
+        if count_of_ryanmen_other_suits >= 2:
+            return False
+
         # let's not go for honitsu nomi
         if not valued_pairs and not valued_pons:
             # this is not honitsu, maybe it will be pinfu one day
@@ -113,6 +125,10 @@ class HonitsuStrategy(BaseStrategy):
         if self.tiles_count_other_suits_not_isolated >= 3:
             # if we don't have pair or pon of honored doras
             if honor_doras_pairs_or_pons == 0:
+                # if we have a ryanmen with dora in other suit and no honor doras, so let's not rush honitsu
+                if count_of_ryanmen_other_suits >= 1 and self.dora_count_other_suits_not_isolated >= 1:
+                    return False
+
                 # we need to either have a valued pair or have at least two honor
                 # pairs to consider honitsu
                 if valued_pairs == 0 and honor_pairs_or_pons < 2:
@@ -136,8 +152,8 @@ class HonitsuStrategy(BaseStrategy):
 
         # if we have a complete set in other suits, we can only throw it away if it's early in the game
         if count_of_shuntsu_other_suits + count_of_koutsu_other_suits >= 1:
-            # too late to throw away chi after 8 step
-            if self.player.round_step > 8:
+            # too late to throw away chi after 5 step
+            if self.player.round_step > 5:
                 return False
 
             # already 1 shanten, no need to throw away complete set
@@ -251,7 +267,7 @@ class HonitsuStrategy(BaseStrategy):
         for x in range(0, len(simple_tiles)):
             tile = simple_tiles[x]
             # we cant build ryanmen with 1 and 9
-            if tile == 1 or tile == 9:
+            if tile == 0 or tile == 8:
                 continue
 
             # bordered tile

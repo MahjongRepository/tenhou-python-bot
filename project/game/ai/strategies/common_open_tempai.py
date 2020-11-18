0da5df6e878a5cda390a06e04052caba28e58e7d
Nihisil <alexey@nihisil.com>
2020-11-18 21:29:15 +0800

    Fix crash with 0 ukeire and zero division

--- project/game/ai/strategies/common_open_tempai.py
+++ project/game/ai/strategies/common_open_tempai.py
@@ -72,6 +72,10 @@ class CommonOpenTempaiStrategy(BaseStrategy):
             )
             return False
 
+        if selected_tile.ukeire == 0:
+            self.player.logger.debug(log.MELD_DEBUG, "Common tempai: 0 ukeire, abort melding", logger_context)
+            return False
+
         if selected_tile.tempai_descriptor["hand_cost"]:
             hand_cost = selected_tile.tempai_descriptor["hand_cost"]
         else:
